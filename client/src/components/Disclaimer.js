3b33ad0758491cc066672d8c26d11b5e3db672e5
GauravWalia19 <gauravwalia019@gmail.com>
2020-09-15 02:25:55 +0530

    completed the main page

---
+++ client/src/components/Disclaimer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Disclaimer = () => {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                bottom: 0,
+                paddingBottom: '5px',
+                width: '100%',
+                textAlign:'center'
+            }}
+        >
+            Disclaimer: This site is fan made.
+        </div>
+    );
+};
+
+export default Disclaimer;
