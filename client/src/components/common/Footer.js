6ad5972df6ac13b8321e43ae6f9cf8b410d1e207
GauravWalia19 <gauravwalia019@gmail.com>
2020-10-04 16:39:49 +0530

    added Footer component

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Footer.css';
+import FooterLink from '../common/FooterLink';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <div className="techousFooter footerContent">
+                <h2>Powered by</h2>
+                <div>
+                    <img src="techousPNG.png" alt="techous logo" />
+                </div>
+            </div>
+            <div className="techousLinks footerContent">
+                <FooterLink
+                    class="fab fa-node-js"
+                    title="Source Code"
+                    link="https://github.com/TechOUs/HacktoberFest20"
+                />
+                <FooterLink
+                    class="fas fa-file-signature"
+                    title="Contribution"
+                    link="https://github.com/TechOUs/HacktoberFest20/tree/master/CONTRIBUTING.md"
+                />
+                <FooterLink class="far fa-file-code" title="Code of Conduct" />
+                <FooterLink class="far fa-comments" title="Our Community" />
+                <FooterLink class="far fa-file-alt" title="LICENSE" />
+            </div>
+            <div id="bottomFooter">
+                <img src="Sponsors.svg" alt="sponsors" />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
